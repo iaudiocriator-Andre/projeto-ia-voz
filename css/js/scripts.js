5ee609a574fbb39edc6bcd7ef5aa210897520ec1
iaudiocriator-Andre <iaudiocriator@gmail.com>
2025-09-09 00:01:04 -0300

    Update scripts.js
    status

--- css/js/scripts.js
+++ css/js/scripts.js
@@ -3,7 +3,7 @@ const voices = [
     { id: 'v_masculina_br', name: 'Gustavo', description: 'Voz masculina profissional e forte.', audio: 'audio/sample_Andre.ogg' }
 ];
 
-const API_URL = 'https://b87f815d79b6.ngrok-free.app'; // 🚨 COLE O SEU NOVO LINK DA API AQUI
+const API_URL = 'https://bb4818f0fa2d.ngrok-free.app'; // 🚨 COLE O SEU NOVO LINK DA API AQUI
 
 const voiceOptionsContainer = document.getElementById('voice-options-container');
 const ttsAudioPlayer = document.getElementById('tts-audio-player');
